454ac6513e8096706f6221e24fd36a18427597be
Marko Jevtic <marjevtic@gmail.com>
2023-05-28 19:34:21 +0200

    Generate red app with models, migrations and registered admin forms

---
+++ red/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import ComputerScienceEducatorsPost, CseEndeavour
+
+admin.register(ComputerScienceEducatorsPost, CseEndeavour)(admin.ModelAdmin)
---
+++ red/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class CseEndeavour(models.Model):
+    label = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.label
+
+
+class ComputerScienceEducatorsPost(models.Model):
+    class PostType(models.TextChoices):
+        QUESTION = 'QS', 'Question'
+        ANSWER = 'AN', 'Answer'
+        COMMENT = 'CM', 'Comment'
+
+    stack_exchange_id = models.IntegerField(unique=True)
+    type = models.CharField(
+        max_length=2,
+        choices=PostType.choices,
+        default=PostType.QUESTION,
+    )
+    text = models.TextField()
+    label = models.ForeignKey(CseEndeavour, on_delete=models.CASCADE)
+    is_deleted = models.BooleanField(default=False)
+
+    @property
+    def unique_id(self):
+        return f'red_${self.id}'
+    
+    def __str__(self):
+        return f'${self.text[:20]}...'
